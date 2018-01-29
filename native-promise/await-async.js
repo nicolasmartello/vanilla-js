a791dedf12572438cd0639a9b4a7abde3efdb3ac
nicolasmartello <martellonicolas@gmail.com>
2018-01-29 14:34:31 -0300

    [VJS-AWAY-ASYNC] Add exercise

---
+++ native-promise/await-async.js
@@ -0,0 +1,30 @@
+add1(10).then(v => {
+  document.getElementById('awaitExample').innerHTML += `--------First promise-------<br>` 
+                                                    + `result: ${v}<br>`;
+});
+
+add2(10).then(v => {
+  document.getElementById('awaitExample').innerHTML += `--------Second promise--------<br>` 
+                                                    + `result: ${v}<br>`;
+});
+
+function resolveAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
+
+async function add1(x) {
+  const a = await resolveAfter2Seconds(20);
+  const b = await resolveAfter2Seconds(30);
+  return x + a + b;
+}
+
+async function add2(x) {
+  const p_a = resolveAfter2Seconds(20);
+  const p_b = resolveAfter2Seconds(30);
+  return x + await p_a + await p_b;
+}
+
